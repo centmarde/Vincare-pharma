f6f4db07ecc677aa052fc82941103b07f2a5df70
TheStrongest <159101935+centmarde@users.noreply.github.com>
2026-07-05 21:56:07 +0800

    fix: ui on stocks

--- src/pages/sales/components/StockTransferCRUD.vue
+++ src/pages/sales/components/StockTransferCRUD.vue
@@ -24,7 +24,7 @@ onMounted(init)
 
 <template>
   <v-container fluid class="pa-2 fill-height align-start">
-    <v-card class="mx-auto w-100" max-width="1400" rounded="lg" elevation="1">
+    <v-card class="mx-auto w-100" rounded="lg" elevation="1">
 
       <!-- Header -->
       <v-card-title class="d-flex justify-space-between align-center pa-5 flex-wrap ga-3">
