2236707c81b3ad9635d4e356258276ef49ecec47
TheStrongest <159101935+centmarde@users.noreply.github.com>
2026-07-11 22:29:18 +0800

    feat: added reorder_no on transactions

--- src/stores/transactionsData.ts
+++ src/stores/transactionsData.ts
@@ -24,6 +24,7 @@ export type TransactionType = {
   id:                  number
   created_at:          string
   reference_no:        string | null
+  reorder_no:          string | null
   transaction_type:    TransactionClass | null
   status:              string | null
   warehouse_id:        number | null
