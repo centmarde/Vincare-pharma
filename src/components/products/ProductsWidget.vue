0cbbb7c0cf9d7ca46a4242221b73a94321e036eb
Britney Spears <159101935+centmarde@users.noreply.github.com>
2026-09-07 21:53:45 +0800

    fix:adding the warehouse filter on the products mobile

--- src/components/products/ProductsWidget.vue
+++ src/components/products/ProductsWidget.vue
@@ -304,6 +304,29 @@ function stockColor(item: any, stock: number) {
 
     <!-- Mobile search -->
     <div v-if="mobile" class="px-3 pb-2">
+      <!-- Warehouse filter -->
+      <v-select
+        v-model="selectedWarehouseId"
+        :items="[{ id: null, name: 'Main Warehouse' }, ...warehousesStore.warehouses]"
+        item-title="name"
+        item-value="id"
+        label="Filter by warehouse..."
+        prepend-inner-icon="mdi-warehouse"
+        variant="outlined"
+        density="compact"
+        hide-details
+        persistent-placeholder
+        @update:model-value="(val) => setWarehouseFilter(val)"
+      >
+        <template #prepend-item>
+          <v-list-item v-if="selectedWarehouseId" @click="setWarehouseFilter(null)">
+            <v-list-item-title class="text-caption text-grey-darken-1"
+              >Clear filter</v-list-item-title
+            >
+          </v-list-item>
+          <v-divider class="mt-2"></v-divider>
+        </template>
+      </v-select>
       <v-text-field
         v-model="searchQuery"
         label="Search products..."
@@ -311,6 +334,7 @@ function stockColor(item: any, stock: number) {
         variant="outlined"
         density="compact"
         hide-details
+        class="mt-2"
         @keyup.enter="handleSearch"
       ></v-text-field>
       <v-text-field
